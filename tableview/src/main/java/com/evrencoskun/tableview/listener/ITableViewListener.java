9b49c0b09e42f6b927d2de2634247f537229c53c
Eugene <45553458+Eugeniydak@users.noreply.github.com>
2021-04-12 23:27:31 +0700

    Update ITableViewListener.java

--- tableview/src/main/java/com/evrencoskun/tableview/listener/ITableViewListener.java
+++ tableview/src/main/java/com/evrencoskun/tableview/listener/ITableViewListener.java
@@ -33,29 +33,29 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public interface ITableViewListener {
 
-    void onCellClicked(@NonNull RecyclerView.ViewHolder cellView, int column, int
-            row);
+    default void onCellClicked(@NonNull RecyclerView.ViewHolder cellView, int column, int
+            row) {}
 
-    void onCellDoubleClicked(@NonNull RecyclerView.ViewHolder cellView, int column, int
-            row);
+    default void onCellDoubleClicked(@NonNull RecyclerView.ViewHolder cellView, int column, int
+            row) {}
 
-    void onCellLongPressed(@NonNull RecyclerView.ViewHolder cellView, int column, int
-            row);
+    default void onCellLongPressed(@NonNull RecyclerView.ViewHolder cellView, int column, int
+            row) {}
 
-    void onColumnHeaderClicked(@NonNull RecyclerView.ViewHolder columnHeaderView, int
-            column);
+    default void onColumnHeaderClicked(@NonNull RecyclerView.ViewHolder columnHeaderView, int
+            column) {}
 
-    void onColumnHeaderDoubleClicked(@NonNull RecyclerView.ViewHolder columnHeaderView, int
-            column);
+    default void onColumnHeaderDoubleClicked(@NonNull RecyclerView.ViewHolder columnHeaderView, int
+            column) {}
 
-    void onColumnHeaderLongPressed(@NonNull RecyclerView.ViewHolder columnHeaderView, int
-            column);
+    default void onColumnHeaderLongPressed(@NonNull RecyclerView.ViewHolder columnHeaderView, int
+            column) {}
 
-    void onRowHeaderClicked(@NonNull RecyclerView.ViewHolder rowHeaderView, int row);
+    default void onRowHeaderClicked(@NonNull RecyclerView.ViewHolder rowHeaderView, int row) {}
 
-    void onRowHeaderDoubleClicked(@NonNull RecyclerView.ViewHolder rowHeaderView, int row);
+    default void onRowHeaderDoubleClicked(@NonNull RecyclerView.ViewHolder rowHeaderView, int row) {}
 
-    void onRowHeaderLongPressed(@NonNull RecyclerView.ViewHolder rowHeaderView, int
-            row);
+    default void onRowHeaderLongPressed(@NonNull RecyclerView.ViewHolder rowHeaderView, int
+            row) {}
 
 }
